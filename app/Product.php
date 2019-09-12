a671741f498467b5bbb608d1f068fa15acd2cd6c
Matheus Dardenne <madsoftworks@outlook.com>
2019-09-12 17:16:33 -0300

    initial commit

---
+++ app/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+	const UNAVAILABLE_PRODUCT = 'unavailable';
+	const AVAILABLE_PRODUCT = 'available';
+    protected $fillable = [
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id',
+	];
+
+	public function isAvailable()
+	{
+		return $this->status == Product::AVAILABLE_PRODUCT;
+	}
+}
